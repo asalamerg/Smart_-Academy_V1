74b40624ba552da60cbf6f2f1f728726b6c54754
Ahmad Maher <asalameh02@gmail.com>
2025-02-13 00:37:32 +0200

    Start Create App Smart Academy

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( SmartAcademy());
+}
+
+class SmartAcademy extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

05aed5c238a8512890b8c1b953f4b82e93ee6bd0
Ahmad Maher <asalameh02@gmail.com>
2025-02-23 18:36:58 +0200

    finish add launcher icon and splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( SmartAcademy());
+  runApp( const SmartAcademy());
 }
 
 class SmartAcademy extends StatelessWidget{
+  const SmartAcademy({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(title: const Text("HOME"),),
+       body: const Center(child: Text("ahmad"),),
+     ),
+   );
   }
 }
